c39797dc72d7edbb5ab85bdacb2b2fcaa8d3fcf3
T Roshan Kumar Patro <patro.roshan1997@gmail.com>
2022-11-12 23:50:11 +0530

    progress added

---
+++ src/com/datamonit_topdog/mainclasses/Main.java
@@ -0,0 +1,20 @@
+package com.datamonit_topdog.mainclasses;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Admin or user");
+		System.out.println("If admin, press 1.");
+		System.out.println("If user, press 2.");
+		int userChoice = sc.nextInt();
+		sc.nextLine();
+		
+		
+
+	}
+
+}
